11b1f258252bc9fd6f8e142dc71bc01e6912094d
agiliswork <49206478+agiliswork@users.noreply.github.com>
2024-09-17 17:07:56 +0500

    Create GetVMKInfoFast.ps1

---
+++ GetVMKInfoFast.ps1
@@ -0,0 +1,45 @@
+function GetVMKInfoFast
+{
+    param(
+    [Parameter()]
+    $esxHost,
+    [ValidateSet('faultToleranceLogging','management','nvmeRdma','nvmeTcp',
+    'ptp','vSphereBackupNFC','vSphereProvisioning','vSphereReplication',
+    'vSphereReplicationNFC','vmotion','vsan','vsanWitness','ALL-VMK-IP')]
+    [string]
+    $nicType
+    )
+
+    if($nicType -eq 'ALL-VMK-IP')
+    {
+        $CustomObjArr = @()
+        $NicConfigObjArr = $esxHost.Config.VirtualNicManagerInfo.NetConfig | Where-Object {$_.SelectedVnic -ne $null } 
+        foreach($NicConfigObj in $NicConfigObjArr)
+        {
+            $seclectedInfo = $NicConfigObj.CandidateVnic | Where-Object {$NicConfigObj.SelectedVnic -contains $_.Key} | Select-Object -ExpandProperty Spec
+            if($null -ne $seclectedInfo)
+            {
+                $CustomObjArr += $seclectedInfo | Select-Object 'Portgroup','Mtu','Mac',
+                                        @{Label='NicType'; Expression={$NicConfigObj.NicType}},
+                                        @{Label='IP'; Expression={$_.Ip.IpAddress}},
+                                        @{Label='SubnetMask'; Expression={$_.Ip.SubnetMask}}
+            } 
+        }
+        return $CustomObjArr
+    }
+    else
+    {
+        $CustomObj = $null
+        $NicConfigObjArr = $esxHost.Config.VirtualNicManagerInfo.NetConfig | Where-Object {$_.NicType -eq $nicType} 
+        $seclectedInfo = $NicConfigObj.CandidateVnic | Where-Object {$NicConfigObj.SelectedVnic -contains $_.Key}  | Select-Object -ExpandProperty Spec
+
+        if($null -ne $seclectedInfo)
+        {
+            $CustomObj = $seclectedInfo | Select-Object 'Portgroup','Mtu','Mac',
+                                @{Label='NicType'; Expression={$nicType}},
+                                @{Label='IP'; Expression={$_.Ip.IpAddress}},
+                                @{Label='SubnetMask'; Expression={$_.Ip.SubnetMask}}
+        }
+        return $CustomObj
+    }
+}
