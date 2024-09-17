ee7171277c29324fc6ccfb3f0716f4178921d886
agiliswork <49206478+agiliswork@users.noreply.github.com>
2024-09-17 07:35:42 +0500

    Update ConnectToVCenter.ps1
    Updated

--- ConnectToVCenter.ps1
+++ ConnectToVCenter.ps1
@@ -1,8 +1,11 @@
 function ConnectToVCenter {
     param (
+        [Parameter(Mandatory=$true)]
+        [Alias("Server")] 
         [string]$vCenterServerName,
         [System.Management.Automation.PSCredential]$credential = $null
     )
+
 
     if ([string]::IsNullOrWhiteSpace($vCenterServerName)) {
         Write-Warning 'ConnectToVCenter: vCenterServerName is NULL or Empty'
@@ -18,14 +21,21 @@ function ConnectToVCenter {
             Write-Error "ConnectToVCenter: Unable to import VMware.VimAutomation.Core module: $_"
         }
     }
+    
+    Write-Host (Get-Module -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue | Format-List -Property Name,Path,Version | Out-String) -ForegroundColor Green 
 
+    Set-PowerCLIConfiguration -Scope Session -ParticipateInCEIP          $false -Confirm:$false -ErrorAction Stop | Out-Null
+    Set-PowerCLIConfiguration -Scope Session -InvalidCertificateAction   Ignore -Confirm:$false -ErrorAction Stop | Out-Null
+    Set-PowerCLIConfiguration -Scope Session -DefaultVIServerMode        Single -Confirm:$false -ErrorAction Stop | Out-Null
+    Set-PowerCLIConfiguration -Scope Session -WebOperationTimeoutSeconds 1800   -Confirm:$false -ErrorAction Stop | Out-Null
 
+    Write-Host "PowerCLIConfiguration:$(Get-PowerCLIConfiguration -Scope Session | Format-List | Out-String )" -ForegroundColor Green
     try {
         if ($credential) {
             $currentConnection = $global:DefaultVIServer | Where-Object { $_.Name -eq $vCenterServerName -and $_.User -match $credential.UserName }
 
             if ($currentConnection.IsConnected) {
-                Write-Host "ConnectToVCenter: Used Connection from global:DefaultVIServers $($global:DefaultVIServer.Name)" -ForegroundColor Green
+                Write-Host "ConnectToVCenter: Used Connection from global:DefaultVIServers $($currentConnection.Name)" -ForegroundColor Green
                 return $currentConnection
             }
 
@@ -36,7 +46,7 @@ function ConnectToVCenter {
             $currentConnection = $global:DefaultVIServer | Where-Object { $_.Name -eq $vCenterServerName -and $_.User -match $env:USERNAME }
 
             if ($currentConnection.IsConnected) {
-                Write-Host "ConnectToVCenter: Used Connection from global:DefaultVIServers $($global:DefaultVIServer.Name)" -ForegroundColor Green
+                Write-Host "ConnectToVCenter: Used Connection from global:DefaultVIServer $($global:DefaultVIServer.Name)" -ForegroundColor Green
                 return $currentConnection
             }
 
@@ -44,16 +54,16 @@ function ConnectToVCenter {
         } 
     }
     catch [VMware.VimAutomation.ViCore.Types.V1.ErrorHandling.InvalidLogin] {
-        Write-Warning "ConnectToVCenter: Invalid credentials or access denied"
+        Write-Warning "ConnectToVCenter: Invalid credentials or access denied, Error: $_"
         return $null
     }
-    catch [System.Management.Automation.RuntimeException] {
-        Write-Warning "ConnectToVCenter: Invalid Host Name"
+    catch [VMware.VimAutomation.Sdk.Types.V1.ErrorHandling.VimException.ViServerConnectionException] {
+        Write-Warning "ConnectToVCenter: Invalid server name or ip, Error: $_"
         return $null
     }
     catch {
         Write-Error "ConnectToVCenter: Connect-VIServer $vCenterServerName Error: $_"
-					
+        return $null		
     }
 
     if ($null -eq $vCenterId) {
@@ -62,7 +72,8 @@ function ConnectToVCenter {
     }
 
     if ($vCenterId.IsConnected) {
-        Write-Host "ConnectToVCenter: $($vCenterId.Name), Connection status: $($vCenterId.IsConnected), Type: $($vCenterId.GetType().FullName)" -ForegroundColor Green
+        $vcPropertyArr = @("Name", "User", "Version", "Build", "IsConnected", "Id", "ServiceUri", "Port")
+        Write-Host "ConnectToVCenter: $($vCenterId | Format-List -Property $vcPropertyArr | Out-String )" -ForegroundColor Green
         return $vCenterId
     }
     else {
